9a707499bd89c5f3be94d83264c8db4029951ca7
Simone <simonepandolfi@protonmail.com>
2025-05-29 01:41:44 +0200

    updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 echo "removing mariadb_data/ content..."
-rm -rf mariadb_data/*
-
-echo "everything has been cleaned up!"
+if rm -rf mariadb_data/*; then
+    echo "everything has been cleaned up!"
+else
+    echo "something went wrong while executing: rm -rf mariadb_data/*"

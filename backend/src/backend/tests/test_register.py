91a4101cdba4eed62023b4e44bb820250c6efd0d
Simone <simonepandolfi@protonmail.com>
2025-06-02 02:43:58 +0200

    aggiunti altri test

--- backend/src/backend/tests/test_register.py
+++ backend/src/backend/tests/test_register.py
@@ -12,3 +12,5 @@ def test_register():
     assert response.status_code == 200
     response = client.post("/register/", json={"user": "Mariano", "email": "marianogiusti@libero.it", "password": "luit1perdon4!"})
     assert response.status_code == 400
+    response = client.post("/register/", json={"user": "hahah", "email": "noemail", "password": "prova"})
+    assert response.status_code == 422
